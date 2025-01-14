1a0605b8759c25db5c067ea8be828ec706e7cc55
Tushar Agrawal <tushar.code.inbox@gmail.com>
2025-01-14 17:21:00 +0530

    configure for gh pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tusharag6.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/codebook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tusharag6', // Usually your GitHub org/user name.
   projectName: 'codebook', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch to deploy to
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
